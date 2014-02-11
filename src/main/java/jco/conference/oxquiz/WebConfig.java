781daed11b1b480f1999d2985216494a03a01895
arawn <arawn.kr@gmail.com>
2014-02-11 13:41:24 +0900

    OXQuiz: 퀴즈(oxquiz.html), 관전자(observer.html) 모드로 분리

--- src/main/java/jco/conference/oxquiz/WebConfig.java
+++ src/main/java/jco/conference/oxquiz/WebConfig.java
@@ -25,6 +25,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/oxquiz.html");
+        registry.addViewController("/observer").setViewName("redirect:/observer.html");
     }
 
     @Override
